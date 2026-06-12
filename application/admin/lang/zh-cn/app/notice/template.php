779d1500da284651e6601cb5b3dfdda24d6feda6
DESKTOP-VLH0JL3\admin <893876967@qq.com>
2026-06-12 10:17:46 +0800

    去除站内信菜单

--- application/admin/lang/zh-cn/app/notice/template.php
+++ application/admin/lang/zh-cn/app/notice/template.php
@@ -7,6 +7,7 @@ return [
     'Content'       => '描述内容',
     'Content placeholder help' => '占位符：{#order}-订单号，{#nickname}-昵称，{#sender}-发送人，{#year}-年，{#month}-月，{#day}-日，{#hour}-时，{#minute}-分，{#second}-秒',
     'Updatetime'    => '更新时间',
+    'Notice letter' => '站内信',
     'Flag userChat' => '用户聊天',
     'Flag c2cChat'  => 'C2C聊天',
     'Flag c2cPaid'  => 'C2C支付',
