56f471dae3761c75ff54af5272aef7e5ddd1ba3c
DESKTOP-VLH0JL3\admin <893876967@qq.com>
2026-09-18 16:01:31 +0800

    接触限制

--- application/config.php
+++ application/config.php
@@ -274,7 +274,7 @@ return [
         //登录验证码
         'login_captcha'         => true,
         //登录失败超过10次则1天后重试
-        'login_failure_retry'   => true,
+        'login_failure_retry'   => false,
         //是否同一账号同一时间只能在一个地方登录
         'login_unique'          => false,
         //是否开启IP变动检测
