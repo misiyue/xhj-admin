8762630e7acbdee722501adf6995526194ee90f7
DESKTOP-VLH0JL3\admin <893876967@qq.com>
2026-09-22 16:29:32 +0800

    虚拟人头像上传oss

--- application/admin/controller/user/Faker.php
+++ application/admin/controller/user/Faker.php
@@ -4,6 +4,8 @@ namespace app\admin\controller\user;
 
 use app\admin\model\UserFaker as UserFakerModel;
 use app\common\controller\Backend;
+use app\common\library\OssStorage;
+use think\Config;
 
 /**
  * 假人管理
@@ -23,9 +25,34 @@ class Faker extends Backend
 
     protected $modelSceneValidate = true;
 
+    protected $noNeedRight = ['uploadOssImage'];
+
     public function _initialize()
     {
         parent::_initialize();
         $this->model = new \app\admin\model\UserFaker;
+    }
+
+    /**
+     * 头像上传至 OSS
+     */
+    public function uploadOssImage()
+    {
+        Config::set('default_return_type', 'json');
+        $file = $this->request->file('file');
+        if (!$file) {
+            $this->error(__('No file upload or server upload limit exceeded'));
+        }
+        try {
+            $oss = new OssStorage();
+            $result = $oss->uploadImage($file, 'user/faker');
+            return json([
+                'code' => 1,
+                'msg'  => __('Uploaded successful'),
+                'data' => $result,
+            ]);
+        } catch (\Exception $e) {
+            $this->error($e->getMessage());
+        }
     }
 }
